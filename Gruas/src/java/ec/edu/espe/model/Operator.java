7d117cfdf010961dbf83376e179692a7078b013a
HENRY ALEXANDER AGUAIZA CHILE <37305587+HenryAlexander123@users.noreply.github.com>
2019-09-29 20:04:10 -0500

    .

---
+++ Gruas/src/java/ec/edu/espe/model/Operator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.model;
+
+/**
+ *
+ * @author henry
+ */
+public class Operator {
+    
+    private String operatorId;
+    private int craneManagerId;
+    private String operatorName;
+    private String operatorLastName;
+    private String operatorLicence;
+    
+    
+    
+}
