04f1c62dd8975557ee5797eff97691804378bee7
CarlosCabascango <cacabascango1@espe.edu.ec>
2019-12-11 01:03:42 -0500

    findClient

--- FrontEnd/modules/clientModule/client.js
+++ FrontEnd/modules/clientModule/client.js
@@ -24,3 +24,30 @@ function cargar(){
             }
         })
 }
+
+function clientById(){
+    var id = document.getElementById('id');
+    const URLAPI = "http://localhost:8080/Crane/Clients/"+id;
+    const container = document.getElementById('tableClient');
+    let contentHTML = '';
+    var i=0;
+    fetch(URLAPI)
+        .then(res => res.json())
+        .then((json)=>{
+            container.innerHTML = '';
+            for(const service of json){
+                i++;
+                container.innerHTML += `
+                <tr>
+                    <th scope="row">${i}</th>
+                    <td>${service.clientId}</td>
+                    <td>${service.clientName}</td>
+                    <td>${service.clientPhone}</td>
+                    <td><a data-toggle='tooltip' data-placement='top' title='Update' style='margin-right:5px' class='btn btn-success btn-sm'>
+                    <i class="material-icons">build</i>
+                    </a></td>
+                </tr>
+                `;
+            }
+        })
+}
