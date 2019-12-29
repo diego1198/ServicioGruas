be3972bc020caade88f9acc63de6c38f0da3f679
HENRY ALEXANDER AGUAIZA CHILE <37305587+HenryAlexander123@users.noreply.github.com>
2019-12-29 17:08:12 -0500

    menu

---
+++ CraneApp/app/src/main/java/ec/edu/espe/craneapp/Menu.java
@@ -0,0 +1,36 @@
+package ec.edu.espe.craneapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Menu extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnOperator;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        btnOperator = findViewById(R.id.btnOperator);
+        btnOperator.setOnClickListener(this);
+    }
+    public void onBackPressed() {
+        finish();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId()==btnOperator.getId()){
+            Intent i = new Intent(getApplicationContext(),Operator.class);
+            startActivity(i);
+            finish();
+        }
+
+    }
+}
---
+++ CraneApp/app/src/main/java/ec/edu/espe/craneapp/Operator.java
@@ -0,0 +1,18 @@
+package ec.edu.espe.craneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class Operator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operator);
+
+    }
+
+
+}
