6f73bda668fed913673e421ac7308698feabfa79
HENRY ALEXANDER AGUAIZA CHILE <37305587+HenryAlexander123@users.noreply.github.com>
2019-12-31 17:03:44 -0500

    interface operator

---
+++ CraneApp/app/src/main/java/ec/edu/espe/craneapp/OperatorListActivity.java
@@ -0,0 +1,14 @@
+package ec.edu.espe.craneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OperatorListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operator_list);
+    }
+}
